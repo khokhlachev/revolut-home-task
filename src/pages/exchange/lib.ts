90f58a2236b971bf3dfa16167b490c92b23ef9c4
Sergey Khokhlachev <sergey@khokhlachev.com>
2021-10-29 11:44:49 +0300

    feat: cap polling requests count

--- src/pages/exchange/lib.ts
+++ src/pages/exchange/lib.ts
@@ -4,29 +4,44 @@ import { ratesSlice } from "@/entities/rates"
 
 export function useRatesPolling(
   currencyCode: CurrencyCode,
-  interval: number = 10000
+  interval: number = 10000,
+  limit: number = 50
 ) {
   const dispatch = useAppDispatch()
 
   useEffect(() => {
+    let count = 1
+
     const action = () => dispatch(ratesSlice.fetchRatesAsync(currencyCode))
     const startPolling = () =>
       setInterval(() => {
+        if (count > limit) {
+          clearInterval(intervalId)
+          return
+        }
+
         promise = action()
+        count++
       }, interval)
     const stopPolling = () => clearInterval(intervalId)
 
     let promise: ReturnType<typeof action> | undefined
     let intervalId = startPolling()
 
-    const handleVisibilityChange = () =>
-      document.hidden ? stopPolling() : (intervalId = startPolling())
+    const handleVisibilityChange = () => {
+      if (count > limit) {
+        document.hidden ? stopPolling() : (intervalId = startPolling())
+      } else {
+        document.removeEventListener("visibilitychange", handleVisibilityChange)
+      }
+    }
 
     document.addEventListener("visibilitychange", handleVisibilityChange)
 
     return () => {
       stopPolling()
+      document.removeEventListener("visibilitychange", handleVisibilityChange)
       promise?.abort()
     }
-  }, [currencyCode, interval, dispatch])
+  }, [currencyCode, interval, dispatch, limit])
 }
